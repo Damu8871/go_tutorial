4a6a6702e9a2cdc5b511dae42c02e6740353310d
damu8871 <damu@git.com>
2019-12-28 13:00:39 +0530

    Slice update

--- slice.go
+++ slice.go
@@ -8,11 +8,31 @@ func SliceBasic() {
 	sli := array[2:4]
 	sli1 := array[1:7]
 	fmt.Println("sli == nil", sli == nil, sli)
+	/*Calling SliceComplete function from this function*/
 	SliceComplete(sli)
 	SliceComplete(sli1)
+
+	/*Calling slice append function from this function*/
+	SliceAppend(array)
 }
 
 /*SliceComplete is a test program function for slice in go*/
 func SliceComplete(slicearr []int) {
 	fmt.Printf("Length of Slice is :%d\n Capacity of Slice is :%d\n The Sliced values are :%v\n", len(slicearr), cap(slicearr), slicearr)
+}
+
+/*SliceAppend is a test program for append in slice*/
+func SliceAppend(arr []int) {
+	sli := arr[3:8]
+	fmt.Printf("before -> array = %v\n", arr)
+	fmt.Printf("before -> sli = %v\n", sli)
+	fmt.Printf("before -> len = %d cap = %d\n", len(sli), cap(sli))
+	fmt.Println("&arr[3] == &sli[0]", &arr[3], &sli[0], &arr[3] == &sli[0])
+
+	/*Appending an array to the sliced array*/
+	sli = append(sli, 10, 20, 30, 40)
+	fmt.Printf("after -> array = %v\n", arr)
+	fmt.Printf("after -> sli = %v\n", sli)
+	fmt.Printf("after -> len = %d cap = %d\n", len(sli), cap(sli))
+	fmt.Println("&arr[3] == &sli[0]", &arr[3], &sli[0], &arr[3] == &sli[0])
 }
